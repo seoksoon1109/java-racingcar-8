c0a10b4864013e5df11184770a07eaba5f9521ef
Seokosoon1109 <hyun_21@naver.com>
2025-10-27 17:39:47 +0900

    feat(model): 자동차 전진 및 이동 로직 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,32 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private static final int MOVE_THRESHOLD = 4;
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move() {
+        int randomValue = Randoms.pickNumberInRange(0, 9);
+        if (randomValue >= MOVE_THRESHOLD) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getPositionBar() {
+        return "-".repeat(position);
+    }
 }
--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,4 +1,32 @@
 package racingcar.model;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Cars {
+    private final List<Car> cars;
+
+    public Cars(String carNamesInput) {
+        this.cars = Arrays.stream(carNamesInput.split(","))
+                .map(String::trim)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public void moveAll() {
+        cars.forEach(Car::move);
+    }
+
+
+    public int getMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
 }
