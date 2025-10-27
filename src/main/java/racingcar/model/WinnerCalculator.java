620325104931b364efbeaeb378622add1459a58f
Seokosoon1109 <hyun_21@naver.com>
2025-10-27 17:41:42 +0900

    feat(model): 우승자 판별 기능 구현

--- src/main/java/racingcar/model/WinnerCalculator.java
+++ src/main/java/racingcar/model/WinnerCalculator.java
@@ -1,4 +1,16 @@
 package racingcar.model;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WinnerCalculator {
+
+    public static List<String> findWinners(Cars cars) {
+        int maxPosition = cars.getMaxPosition();
+
+        return cars.getCars().stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
 }
