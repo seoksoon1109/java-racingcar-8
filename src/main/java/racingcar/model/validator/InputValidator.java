95f8f86025dfc3055bf627d5ef038a212fc96b78
Seokosoon1109 <hyun_21@naver.com>
2025-10-27 17:32:10 +0900

    feat(view): 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/model/validator/InputValidator.java
+++ src/main/java/racingcar/model/validator/InputValidator.java
@@ -1,4 +1,32 @@
 package racingcar.model.validator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InputValidator {
+
+    public static void validateCarNames(String input){
+        if(input == null || input.isBlank()){
+            throw new IllegalArgumentException("자동차 이름을 입력하세요.");
+        }
+
+        String[] names = input.split(",");
+        Set<String> uniqueNames = new HashSet<>();
+
+        for (String rawName : names) {
+            String name = rawName.trim();
+
+            if (name.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 공백으로 설정할 수 없습니다!");
+            }
+
+            if (name.length() < 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+
+            if (!uniqueNames.add(name)){
+                throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+            }
+        }
+    }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,13 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.validator.InputValidator;
+
 public class InputView {
+    public static String readCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        InputValidator.validateCarNames(input);
+        return input;
+    }
 }
